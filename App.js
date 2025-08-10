668161f80e9376739df4165f74d8b772452083c2
hansrajpatel <hansraj7222@gmail.com>
2025-08-10 18:47:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignupScreen from './screens/SignupScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Signup" component={SignupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
